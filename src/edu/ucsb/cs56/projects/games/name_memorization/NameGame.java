c09269007a8405708e7794ff96be1f3090593985
Anthony Hoang <ant.hoang93@gmail.com>
2014-02-22 15:49:31 -0800

    added all 5 buttons to the north of the jframe

--- src/edu/ucsb/cs56/projects/games/name_memorization/NameGame.java
+++ src/edu/ucsb/cs56/projects/games/name_memorization/NameGame.java
@@ -5,10 +5,11 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
+import javax.swing.*;
   /**
    * Preliminary engine for running a name memorization game
    *
@@ -16,21 +17,26 @@ import java.awt.BorderLayout;
    *@version  for cs56, Spring 2014
    */
 public class NameGame extends JFrame{
-    private JButton save;
+    private JButton edit;
+    private JButton add;
+    private JButton delete;
     private JButton next;
     private JButton previous;
     private DirectoryLister dir;
     private int current;
     private Image pic;
+    private JPanel north;
 
 
     public void paint(Graphics g){
+	/*
         //get the picture to paint
         pic = Toolkit.getDefaultToolkit().getImage(dir.getFullFilenames().get(current));
         //call super's paint method
         super.paint(g);
         //draw designated picture in background
         g.drawImage(pic, 115, 90,300, 300, this);
+	*/
     }
 
     /**
@@ -41,20 +47,29 @@ public class NameGame extends JFrame{
         dir = new DirectoryLister("src/people");
         current = 0;
         this.getContentPane().setLayout(new BorderLayout());
-
+	
+	north = new JPanel();
 
         HandlerClassN handlerN = new HandlerClassN();
         next = new JButton("next");	
         next.addActionListener(handlerN);
-	this.add(next, BorderLayout.EAST);
+       
 
 	HandlerClassP handlerP = new HandlerClassP();
 	previous = new JButton("previous");
 	previous.addActionListener(handlerP);
-	this.add(previous,BorderLayout.WEST);
+	
 
-	save = new JButton("save");
-	this.add(save,BorderLayout.NORTH);
+	edit = new JButton("edit");
+	add = new JButton("add");
+	delete = new JButton("delete");
+	
+	north.add(edit);
+	north.add(add);
+	north.add(delete);
+	north.add(previous);
+	north.add(next);
+	this.add(north, BorderLayout.NORTH);
 	
     }
 
