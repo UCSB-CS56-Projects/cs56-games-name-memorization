3330a39aef56b9bae2da420368e0cdfac7de3efe
IlMatteo <MatthewPontarolo@gmail.com>
2018-02-26 22:39:55 -0800

    Refactoring and making Card use FX

--- src/edu/ucsb/cs56/projects/games/name_memorization/DeckTest.java
+++ src/edu/ucsb/cs56/projects/games/name_memorization/DeckTest.java
@@ -12,9 +12,9 @@ public class DeckTest {
     private Card c2;
 
     @Before public void setUp() {
-      d1 = new Deck("d1");
-	    c1 = new Card("side1", "side2", false);
-      c2 = new Card("edit1", "edit2", false);
+        d1 = new Deck("d1");
+        c1 = new Card("side1", "side2", false);
+        c2 = new Card("edit1", "edit2", false);
     }
 
     @Test
@@ -24,19 +24,19 @@ public class DeckTest {
 
     @Test
     public void test_addCard() {
-      d1.addCard("side1", "side2", false);
-	    assertEquals("side1", d1.get(0).getSide1());
-      assertEquals("side2", d1.get(0).getSide2());
-      assertEquals(false, d1.get(0).isPic());
+        d1.addCard("side1", "side2", false);
+        assertEquals("side1", d1.get(0).getSide1());
+        assertEquals("side2", d1.get(0).getSide2());
+        assertEquals(false, d1.get(0).isPic());
     }
 
     @Test
     public void test_editCard() {
-      d1.add(0,c1);
-      d1.editCard(d1.get(0), "edit1", "edit2");
-	    assertEquals("edit1", d1.get(0).getSide1());
-      assertEquals("edit2", d1.get(0).getSide2());
-      assertEquals(false, d1.get(0).isPic());
+        d1.add(0,c1);
+        d1.editCard(d1.get(0), "edit1", "edit2");
+        assertEquals("edit1", d1.get(0).getSide1());
+        assertEquals("edit2", d1.get(0).getSide2());
+        assertEquals(false, d1.get(0).isPic());
     }
 
 }
