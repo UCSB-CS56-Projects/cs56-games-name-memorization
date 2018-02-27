17c0792c784955da05620cba511bb215558ad171
IlMatteo <MatthewPontarolo@gmail.com>
2018-02-26 22:32:47 -0800

    Finished up uploadHandler in TestPane

--- src/edu/ucsb/cs56/projects/games/name_memorization/TestPane.java
+++ src/edu/ucsb/cs56/projects/games/name_memorization/TestPane.java
@@ -13,6 +13,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
